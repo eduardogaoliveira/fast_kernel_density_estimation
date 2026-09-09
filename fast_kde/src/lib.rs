6f179d5c30290f8978a214eed4626a38c1990675
code-Cells <eduardo.g.amaral1997@gmail.com>
2026-09-09 16:20:59 -0300

    Implemented KDE Deriche 2D

--- fast_kde/src/lib.rs
+++ fast_kde/src/lib.rs
@@ -478,10 +478,529 @@ fn kde_mode_deriche<'py>(
     }
 }
 
+/// Performs 2D linear binning of the input data over the specified range and
+/// number of bins.
+/// 
+/// This method is based on the approach described in Section 5,
+/// "Data Binning," of the doi:10.1007/978-3-319-71688-6_5."
+/// The weight of each data point is distributed proportionally between its
+/// four neighboring bins.
+///
+/// The binning operation is automatically parallelized using the Rayon crate.
+///
+/// ## Arguments
+/// - x: One-dimensional data slice to be binned.
+/// - y: One-dimensional data slice to be binned.
+/// - xmin: Minimum value of the x data range.
+/// - xmax: Maximum value of the x data range.
+/// - ymin: Minimum value of the y data range.
+/// - ymax: Maximum value of the y data range.
+/// - xbins: Number of bins alongside x axis.
+/// - ybins: Number of bins alongside y axis.
+///
+/// ## Returns
+/// A Vec<Vec<f64>> containing the count assigned to each bin.
+fn linear_binning_2d(
+    x: &[f64],
+    y: &[f64],
+    xmin: f64,
+    xmax: f64,
+    ymin: f64,
+    ymax: f64,
+    xbins: usize,
+    ybins: usize,
+) -> Vec<Vec<f64>> {
+    if xbins == 0 || ybins == 0 {
+        return vec![];
+    }
+
+    if x.is_empty() || y.is_empty() {
+        return vec![vec![0.0; ybins]; xbins];
+    }
+
+    let xbin_width = (xmax - xmin) / xbins as f64;
+    let ybin_width = (ymax - ymin) / ybins as f64;
+
+    if xbin_width.abs() < 1e-9 || ybin_width.abs() < 1e-9 {
+        return vec![vec![0.0; ybins]; xbins];
+    }
+
+    assert_eq!(
+        x.len(),
+        y.len(),
+        "x and y must contain the same number of points"
+    );
+
+    let num_threads = rayon::current_num_threads();
+    let chunk_size = (x.len() + num_threads - 1) / num_threads;
+    let thread_hists: Vec<Vec<f64>> = x
+        .par_chunks(chunk_size)
+        .zip(y.par_chunks(chunk_size))
+        .map(|(x_chunk, y_chunk)| {
+            let mut local_hist = vec![0.0_f64; xbins * ybins];
+            for (&x_val, &y_val) in x_chunk.iter().zip(y_chunk.iter()) {
+                if x_val >= xmin && x_val < xmax && 
+                   y_val >= ymin && y_val < ymax {
+                    let posh = (x_val - xmin) / xbin_width;
+                    let posv = (y_val - ymin) / ybin_width;
+                    let kh = posh.floor() as usize;
+                    let kv = posv.floor() as usize;
+                    let fraction_left = 1.0 - (posh - kh as f64);
+                    let fraction_right = posh - kh as f64;
+                    let fraction_down = 1.0 - (posv - kv as f64);
+                    let fraction_up = posv - kv as f64;
+                    let a = fraction_up * fraction_left;
+                    let b = fraction_up * fraction_right;
+                    let c = fraction_down * fraction_left;
+                    let d = fraction_down * fraction_right;
+                    let total_area = a + b + c + d;
+                    if kh < xbins && kv < ybins {
+                        if kh + 1 < xbins && kv + 1 < ybins {
+                            local_hist[kh * ybins + kv] += c / total_area;
+                            local_hist[(kh+1) * ybins + kv] += d / total_area;
+                            local_hist[kh * ybins + (kv+1)] += a / total_area;
+                            local_hist[(kh+1) * ybins + (kv+1)] += b / total_area;
+                        }
+                    }
+                } else if (x_val - xmax).abs() < 1e-9 && xbins > 0 &&
+                          (y_val - ymax).abs() < 1e-9 && ybins > 0 {
+                    local_hist[(xbins - 1) * ybins + (ybins - 1)] += 1.0;
+                }
+            }
+            local_hist
+        })
+        .collect();
+
+    let mut global_hist = vec![vec![0.0_f64; ybins]; xbins];
+    for local_hist in thread_hists {
+        for i in 0..xbins {
+            for j in 0..ybins {
+                global_hist[i][j] += local_hist[i * ybins + j];
+            }
+        }
+    }
+    global_hist
+}
+
+/// # 2D Kernel Density Estimation (KDE) Using 2D Linear Binning and the 
+/// second order approximation Deriche Filter
+///
+/// For a two-dimensional dataset, a histogram grid is first constructed using
+/// 2D linear binning. KDE is then computed by approximating Gaussian smoothing
+/// with a recursive Deriche filter over each 1D axis, following the separability
+/// condition of the gaussian kernel. The bandwitdh for each axis is automatically
+/// calculated using the Scott's factor.
+/// The final result is normalized as a probability density function (PDF).
+/// A 0.5*sigma tail is added to each of the grid's edges.
+///
+/// ## Arguments
+/// - py: Python GIL token. Required for handling Python objects in PyO3 functions.
+/// - data: Input data (numpy.ndarray containing a one-dimensional f64 array).
+/// - bins: Number of bins at which the KDE is evaluated (number of grid points).
+///
+/// ## Returns
+/// - PyResult<(Bound<'py, PyArray1<f64>>, Bound<'py, PyArray1<f64>>,
+///             Bound<'py, PyArray2<f64>>)>:
+/// - First element: X-coordinates of the bin centers (numpy.ndarray).
+/// - Second element: Y-coordinates of the bin centers (numpy.ndarray).
+/// - Third element: Corresponding PDF values for each bin (numpy.ndarray).
+///
+/// ## Errors
+/// - PyValueError:
+/// - If any dataset dimension contains fewer than 3 samples.
+/// - If any number of bins is zero.
+/// - If xmax is smaller than xmin (invalid data range).
+/// - If ymax is smaller than ymin (invalid data range).
+/// - If either bin width dx or dy is extremely small or zero.
+/// - If the normalization factor is close to zero while the sum of the
+/// filtered histogram is nonzero.
+#[pyfunction]
+fn kde_deriche_2d<'py>(
+    py: Python<'py>,
+    data: PyReadonlyArray2<'py, f64>,
+    bins: usize, // Square binning-only. Temporary
+) -> PyResult<(
+    Bound<'py, PyArray1<f64>>, 
+    Bound<'py, PyArray1<f64>>, 
+    Bound<'py, PyArray2<f64>>,
+    Bound<'py, PyArray2<f64>>,
+    f64
+)> {
+    let view = data.as_array();
+    let shape = view.shape();
+    let (x_slice, y_slice);
+    let (x_vec, y_vec); // Temporary storage if transposed array isn't contiguous
+    if shape.len() != 2 {
+        return Err(PyValueError::new_err("Input array must be 2-dimensional."));
+    }
+    let xbinding = view.row(0);
+    let ybinding = view.row(1);
+    if shape[0] == 2 {
+        // Shape is (2, N)
+        x_slice = xbinding.as_slice().ok_or_else(|| {
+            PyValueError::new_err("x row is not contiguous")
+        })?;
+        y_slice = ybinding.as_slice().ok_or_else(|| {
+            PyValueError::new_err("y row is not contiguous")
+        })?;
+    } else if shape[1] == 2 {
+        // Shape is (N, 2)
+        x_vec = view.column(0).to_vec();
+        y_vec = view.column(1).to_vec();
+        x_slice = &x_vec;
+        y_slice = &y_vec;
+    } else {
+        return Err(PyValueError::new_err(
+            "Data shape must be either (2, N) or (N, 2).",
+        ));
+    }
+    let x = x_slice;
+    let y = y_slice;
+    let xbins = bins;
+    let ybins = bins;
+
+    if x.len() < 4 || y.len() < 4 {
+        return Err(PyValueError::new_err(
+            "Need at least 4 samples per dimension for KDE 2D."
+        ));
+    }
+    if xbins == 0 || ybins == 0 {
+        return Err(PyValueError::new_err(
+            "Number of bins must be greater than 0 in all dimensions.",
+        ));
+    }
+    let h = bandwidth_matrix(&x, &y);
+    let (_p, d, _pt) = decompose_matrix(&h);
+    let sigma_x = d[0][0].sqrt();
+    let sigma_y = d[1][1].sqrt();
+
+    let xmin = x.iter().cloned().fold(f64::INFINITY, f64::min) -0.5*sigma_x;
+    let xmax = x.iter().cloned().fold(f64::NEG_INFINITY, f64::max) +0.5*sigma_x;
+    let ymin = y.iter().cloned().fold(f64::INFINITY, f64::min) -0.5*sigma_y;
+    let ymax = y.iter().cloned().fold(f64::NEG_INFINITY, f64::max) +0.5*sigma_y;
+
+    if xmax < xmin {
+        return Err(PyValueError::new_err(format!(
+            "xmax ({}) must be greater than or equal to xmin ({})",
+            xmax, xmin
+        )));
+    }
+    if ymax < ymin {
+        return Err(PyValueError::new_err(format!(
+            "ymax ({}) must be greater than or equal to ymin ({})",
+            ymax, ymin
+        )));
+    }
+
+    let mut hist_counts = linear_binning_2d(
+        x, y, xmin, xmax, ymin, ymax, bins, bins);
+    // if hist_counts.iter().flatten().all(|&h_val| h_val.abs() < 1e-9) {
+    //     todo!()
+    // }
+    let bin_range_x = xmax - xmin;
+    let bin_range_y = ymax - ymin;
+    
+    // Compute bin scale factors (bins per data unit)
+    let sfx = xbins as f64 / bin_range_x;
+    let sfy = ybins as f64 / bin_range_y;
+
+    // Standard deviation in bins: sqrt(bandwidth_variance) * bins_per_unit
+    let deriche_sigma_in_bins_x = sigma_x * sfx;
+    let deriche_sigma_in_bins_y = sigma_y * sfy;
+
+    // 1. Filter along Y direction (for each fixed X index i)
+    // hist_counts is Vec<Vec<f64>> of shape [xbins][ybins]
+    hist_counts.par_iter_mut().for_each(|x_row| {
+        // x_row has length ybins
+        deriche_recursive_filter_2nd_order_approx(x_row, deriche_sigma_in_bins_y);
+    });
+
+    // 2. Filter along X direction (for each fixed Y index j)
+    for j in 0..ybins {
+        let mut column: Vec<f64> = (0..xbins).map(|i| hist_counts[i][j]).collect();
+
+        deriche_recursive_filter_2nd_order_approx(&mut column, deriche_sigma_in_bins_x);
+
+        for i in 0..xbins {
+            hist_counts[i][j] = column[i];
+        }
+    }
+
+    // 3. PDF
+    let sum_of_filtered_hist: f64 = hist_counts.iter().flatten().sum();
+    let dx = (xmax - xmin) / xbins as f64;
+    let dy = (ymax - ymin) / ybins as f64;
+
+    if dx <= 0.0 || dy <= 0.0 {
+        return Err(PyValueError::new_err(
+            "Bin widths (dx, dy) must not be too small or zero."
+        ));
+    }
+
+    let normalization_factor = sum_of_filtered_hist * dx * dy;
+
+    if normalization_factor.abs() < 1e-9 {
+        return Err(PyValueError::new_err(
+            "Normalization factor is near zero despite non-zero filtered histogram sum.",
+        ));
+    }
+
+    for val in hist_counts.iter_mut().flatten() {
+        *val /= normalization_factor;
+    }
+
+    let x_coords: Vec<f64> = (0..xbins).map(|i| xmin + (i as f64 + 0.5) * dx).collect();
+    let y_coords: Vec<f64> = (0..ybins).map(|i| ymin + (i as f64 + 0.5) * dy).collect();
+
+    let x_py_bound = PyArray1::from_vec(py, x_coords);
+    let t_py_bound = PyArray1::from_vec(py, y_coords);
+    let pdf_py_bound = PyArray2::from_vec2(py, &hist_counts)?;
+    let cov: Vec<Vec<f64>> = covariance_2d(&x, &y, false)
+        .iter()
+        .map(|row| row.to_vec())
+        .collect();
+    let py_cov = PyArray2::from_vec2(py, &cov)?;
+    let h_vec: Vec<Vec<f64>> = h
+        .iter()
+        .map(|row| row.to_vec())
+        .collect();
+    let py_h = PyArray2::from_vec2(py, &h_vec)?;
+
+    Ok((x_py_bound, t_py_bound, pdf_py_bound, py_h, scotts_factor(&x)))
+}
+
+/// # Uses a second-order filter for 1-dimentional recursion
+/// 
+/// Described by Deriche (1990) in doi:10.1109/34.41386
+/// alpha definition is described by Yang (2012) in doi:10.1007/978-3-642-33718-5_29
+/// 
+/// ## Arguments
+/// - signal: Any dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+/// Modified in place.
+/// - sigma: Axis bandwidth multiplied by the bin scale factors
+fn deriche_recursive_filter_2nd_order_approx(
+    signal: &mut [f64], 
+    sigma: f64) 
+{
+    if sigma.abs() < 1e-9 || signal.is_empty() {
+        return;
+    }
+
+    let n = signal.len();
+    let alpha = 1.695 / sigma;
+    let ea = (-alpha).exp();
+    let e2a = (-2.0 * alpha).exp();
+
+    // 2nd-order Deriche filter coefficients (Gaussian approximation)
+    let k = (1.0 - ea) * (1.0 - ea) / (1.0 + 2.0 * alpha * ea - e2a);
+    let a1_plus = k;
+    let a2_plus = k * ea * (alpha - 1.0);
+    let a3_minus = k * ea * (alpha + 1.0);
+    let a4_minus = -k * e2a;
+
+    let b1 = 2.0 * ea;
+    let b2 = -e2a;
+
+    let mut y_plus = vec![0.0; n];
+    let mut y_minus = vec![0.0; n];
+
+    // 1. Causal (Forward) Pass
+    y_plus[0] = a1_plus * signal[0];
+    if n > 1 {
+        y_plus[1] = a1_plus * signal[1] + a2_plus * signal[0] + b1 * y_plus[0];
+    }
+    for i in 2..n {
+        y_plus[i] = a1_plus * signal[i] + a2_plus * signal[i - 1] 
+                  + b1 * y_plus[i - 1] + b2 * y_plus[i - 2];
+    }
+
+    // 2. Anti-Causal Pass
+    y_minus[n - 1] = 0.0;
+    if n > 1 {
+        y_minus[n - 2] = a3_minus * signal[n - 1];
+    }
+    if n > 2 {
+        for i in (0..n - 2).rev() {
+            y_minus[i] = a3_minus * signal[i + 1] + a4_minus * signal[i + 2] 
+                       + b1 * y_minus[i + 1] + b2 * y_minus[i + 2];
+        }
+    }
+
+    // 3. Combination of passes
+    for i in 0..n {
+        signal[i] = y_plus[i] + y_minus[i];
+    }
+}
+
+/// # Calculates Scott's factor for the input array
+///
+/// ## Arguments
+/// - x: Any dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+///
+/// ## Returns
+/// - f64
+fn scotts_factor(
+    x: &[f64],
+) -> f64 {
+    let d: f64 = 2.;
+    let n: f64 = x.len() as f64;
+    n.powf(-1. / (4. + d))
+}
+
+/// # Calculates average value of 1D array
+///
+/// ## Arguments
+/// - x: Any dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+///
+/// ## Returns
+/// - f64
+fn mean(
+    x: &[f64]
+) -> f64 {
+    let n = x.len() as f64;
+    let sum: f64 = x.iter().sum();
+    sum / n
+}
+
+/// # Calculates covariance matrix of the input arrays
+///
+/// ## Arguments
+/// - x: First dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+/// - y: Second dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+///
+/// ## Returns
+/// - [[f64; 2]; 2]
+fn covariance_2d(
+    x: &[f64],
+    y: &[f64],
+    population: bool
+) -> [[f64; 2]; 2] {
+    assert! (x.len() == y.len());
+    let n = x.len() as f64;
+    let divisor = if population {
+        n as f64
+    } else {
+        (n - 1.) as f64
+    };
+    let mut cov = [[0.; 2]; 2];
+    let x_mean = mean(&x);
+    let y_mean = mean(&y);
+    for (x, y) in x.iter().zip(y.iter()) {
+        let x_diff = x - x_mean;
+        let y_diff = y - y_mean;
+        cov[0][0] += x_diff * x_diff;
+        cov[0][1] += x_diff * y_diff;
+        cov[1][1] += y_diff * y_diff;
+    }
+    cov[0][0] /= divisor;
+    cov[0][1] /= divisor;
+    cov[1][0] = cov[0][1];
+    cov[1][1] /= divisor;
+    cov
+}
+
+/// # Calculates the bandwidth matrix given the input arrays
+///
+/// ## Arguments
+/// - x: First dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+/// - y: Second dimention of input data (numpy.ndarray containing a one-dimensional f64 array).
+///
+/// ## Returns
+/// - [[f64; 2]; 2]
+fn bandwidth_matrix(
+    x: &[f64],
+    y: &[f64],
+) -> [[f64; 2]; 2] {
+    let f = scotts_factor(x);
+    let mut h = covariance_2d(x, y, false);
+    h.iter_mut()
+        .flatten()
+        .for_each(|val| *val *= f * f);
+    h
+}
+
+/// # Decompose a square array A into it's eigenvectors and eigenvalues, following
+/// A = PDP^T where P^T is P transposed 
+///
+/// ## Arguments
+/// - matrix: 2x2 array.
+///
+/// ## Returns
+/// - ([[f64; 2]; 2], [[f64; 2]; 2], [[f64; 2]; 2])
+/// - First element: P
+/// - Second element: D
+/// - Third element: P^T
+fn decompose_matrix(
+    matrix: &[[f64; 2]; 2] 
+) -> ([[f64; 2]; 2], [[f64; 2]; 2], [[f64; 2]; 2]) {
+    let a = matrix[0][0];
+    let b0 = matrix[0][1];
+    let b1 = matrix[1][0];
+    let c = matrix[1][1];
+    // det(A-lI) = a*c -(a+c)l + l^2 = 0
+    let apc = a + c;
+    let delta = (apc*apc - 4.*(a*c-b0*b1)).sqrt();
+    let lambda_1 = 0.5 * (apc + delta);
+    let lambda_2 = 0.5 * (apc - delta);
+    let p = [[-b0/(a-lambda_2), -b0/(a-lambda_1)], [1., 1.]];
+    let d = [[lambda_2, 0.], [0., lambda_1]];
+    let pt = [[-b0/(a-lambda_2), 1.], [-b0/(a-lambda_1), 1.]];
+    (p, d, pt)
+}
+
+/// # Perform matricial multiplication between two 2x2 arrays
+///
+/// ## Arguments
+/// - a: 2x2 array.
+/// - b: 2x2 array.
+///
+/// ## Returns
+/// - [[f64; 2]; 2]
+fn matrix_multiplication(
+    a: &[[f64; 2]; 2],
+    b: &[[f64; 2]; 2] 
+) -> [[f64; 2]; 2] {
+    return [
+        [a[0][0]*b[0][0] + a[0][1]*b[1][0], a[0][0]*b[0][1] + a[0][1]*b[1][1]],
+        [a[1][0]*b[0][0] + a[1][1]*b[1][0], a[1][0]*b[0][1] + a[1][1]*b[1][1]]
+    ]
+}
+
+/// # Takes the square root of every array element
+///
+/// ## Arguments
+/// - matrix: 2x2 array.
+///
+/// ## Returns
+/// - [[f64; 2]; 2]
+fn matrix_sqrt(
+    matrix: &[[f64; 2]; 2]
+) -> [[f64; 2]; 2] {
+    let sqrt_matrix = [
+        [matrix[0][0].sqrt(), matrix[0][1].sqrt()],
+        [matrix[1][0].sqrt(), matrix[1][1].sqrt()]
+    ];
+    sqrt_matrix
+}
+
+/// # Calculates the determinant of the input matrix
+///
+/// ## Arguments
+/// - a: 2x2 array.
+///
+/// ## Returns
+/// - f64
+fn det(
+    a: &[[f64; 2]; 2]
+) -> f64 {
+    a[0][0] * a[1][1] - a[0][1] * a[1][0]
+}
+
 /// # Pythonモジュール `fast_kde` の定義
 #[pymodule]
 fn fast_kde(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(kde_deriche, m)?)?;
+    m.add_function(wrap_pyfunction!(kde_deriche_2d, m)?)?;
     m.add_function(wrap_pyfunction!(kde_mode_deriche, m)?)?;
     Ok(())
 }
